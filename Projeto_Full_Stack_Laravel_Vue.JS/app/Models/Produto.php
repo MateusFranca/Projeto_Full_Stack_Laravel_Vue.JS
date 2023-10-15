193ca9221d3f3f5b8572288253372f64b1d3655c
Mateus F. Cunha <101605976+MateusFranca@users.noreply.github.com>
2023-10-14 23:09:55 -0300

    Melhoria no Modal Produto

--- Projeto_Full_Stack_Laravel_Vue.JS/app/Models/Produto.php
+++ Projeto_Full_Stack_Laravel_Vue.JS/app/Models/Produto.php
@@ -23,7 +23,7 @@ class Produto extends Model
             'nome' => 'required|min:3|max:50',
             'descricao' => 'required|string|max:2000',
             'categoria' => 'nullable|string|max:50',
-            'imagem' => 'required|string',
+            'imagem' => 'required|image|mimes:jpeg,png,jpg',
             'preco' => 'required|numeric|min:0',
             'disponivel' => 'boolean',
         ];
@@ -34,6 +34,7 @@ class Produto extends Model
             'required' => 'O campo :attribute é obrigatório.',
             'max' => 'O campo :attribute não pode ter mais de :max caracteres.',
             'numeric' => 'O campo :attribute deve ser um número.',
+            'image' => 'O campo :attribute deve ser uma imagem nos formatos JPEG, PNG ou JPG.',
             'min' => 'O campo :attribute deve ser no mínimo :min.',
             'boolean' => 'O campo :attribute deve ser verdadeiro ou falso.',
         ];
